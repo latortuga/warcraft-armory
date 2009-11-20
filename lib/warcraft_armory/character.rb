f31334872cb441ed542776a17b27d6d4ab1e2078
Drew Ulmer <latortuga@gmail.com>
2009-11-19 22:49:33 -0600

    Added Arena Team parsing from armory. Arena teams are not automatically loaded when a character is searched for, rather they are loaded on demand when character.arena_teams is called. character.arena_teams is a hash with three available keys: :twos, :threes, and :fives, each of which is an ArenaTeam object replete with attributes describing the team. Added a second config file to aid in the retrieval of arena team information.

--- lib/warcraft_armory/character.rb
+++ lib/warcraft_armory/character.rb
@@ -1,5 +1,9 @@
 module WarcraftArmory 
   
+  # Load the XML Mappings from the supplied config.yml file.
+  CONFIG = YAML.load_file("#{GEM_ROOT}/config/config.yml")["character"]
+  CONFIG_ARENA = YAML.load_file("#{GEM_ROOT}/config/config_arena.yml")["arena_teams"]
+
   # Gives you access to Character information
   #
   # ==== Usage
@@ -47,13 +51,16 @@ module WarcraftArmory
   #   # => "Level 48 Human Warrior"
   #
   class Character
-    # Load the XML Mappings from the supplied config.yml file.
-    CONFIG = YAML.load_file("#{GEM_ROOT}/config/config.yml")["character"]
+    attr_accessor :locale
     
     CONFIG["attributes"].each_pair do |key, value|
       attr_accessor "#{key}".to_sym
     end
-    
+
+    CONFIG["attributes"].each_pair do |key, value|
+      attr_accessor "#{key}".to_sym
+    end
+
     # Returns the full name of the character, including the prefix and suffix.
     #
     #   character.full_name
@@ -69,6 +76,29 @@ module WarcraftArmory
     def description
       "Level #{level} #{race} #{class_name}"
     end
+
+    def arena_teams
+      load_arena_teams if !@arena_teams_hash
+      @arena_teams_hash
+    end
+
+    def load_arena_teams
+      result = {
+        :twos   => WarcraftArmory::ArenaTeam.new,
+        :threes => WarcraftArmory::ArenaTeam.new,
+        :fives  => WarcraftArmory::ArenaTeam.new
+      }
+      url = WarcraftArmory::Base.generate_url(@locale, @realm, @name, CONFIG_ARENA["file"])
+      doc = WarcraftArmory::Utils::Parser.parse_url(url)
+      { :twos => 2, :threes => 3, :fives => 5 }.each do |team_type, size|
+        CONFIG_ARENA["attributes"].each_pair do |key, code|
+          if ((doc/"arenaTeam[@teamSize=#{size}]").length > 0)
+            result[team_type].send("#{key}=".to_sym, eval(code.gsub(':size',size.to_s)))
+          end
+        end
+      end
+      @arena_teams_hash = result
+    end
     
     # Finds a World of Warcraft character.
     # * <tt>location</tt> - A symbol specifying your realm's location. E.g. <tt>:eu</tt> or <tt>:us</tt>
@@ -79,6 +109,7 @@ module WarcraftArmory
     #  # => <WarcraftArmory::Character>
     def self.find(location, realm, character)
       result = WarcraftArmory::Character.new
+      result.locale = location
       
       url = WarcraftArmory::Base.generate_url(location, realm, character, CONFIG["file"])
       doc = WarcraftArmory::Utils::Parser.parse_url(url)
@@ -89,5 +120,21 @@ module WarcraftArmory
       
       return result
     end    
+  end
+
+  class ArenaTeam
+    attr_accessor :size, :members, :games_played
+
+    CONFIG_ARENA["attributes"].each_pair do |key, value|
+      attr_accessor "#{key}".to_sym
+    end
+
+    def season_record
+      "#{season_games_won} / #{season_games_played}"
+    end
+
+    def season_win_percentage
+      (season_games_won / season_games_played.to_f*100).to_i
+    end
   end
 end
